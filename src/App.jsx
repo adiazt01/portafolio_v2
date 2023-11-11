527bef4bfcacdeac2487cdff3ceb65d98cd56abc
adiazt01 <adiaztovar2004@gmail.com>
2023-11-11 16:35:30 -0400

    Add Technologies section to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { FullWidthLayout } from "./components/layouts/FullWidthLayout";
 import { Hero } from "./components/sections/Hero";
+import { Technologies } from "./components/sections/Technologies";
 
 function App() {
   return (
     <>
       <FullWidthLayout>
         <Hero />
+        <Technologies />
       </FullWidthLayout>
     </>
   );
